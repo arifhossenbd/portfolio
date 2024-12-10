66a2a28f4a453388f66cd55bee27baeeddd23685
Arif Hossen <arifhossengd@gmail.com>
2024-12-10 18:46:21 +0600

    NavLinks bg hover remove and font-semibold added

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function generateNavLinks(linkTexts, targetElementId) {
     const targetElement = document.getElementById(targetElementId);
     if (targetElement) {
         targetElement.innerHTML = linkTexts.map((text) => {
-            return `<li class="text-emerald-700 hover:text-emerald-900 transition-all duration-300 ease-in-out hover:translate-x-1">
-                        <a class="flex items-center gap-1" href="#${text.toLowerCase()}">
+            return `<li class="text-emerald-700 hover:text-emerald-900 transition-all duration-300 ease-in-out hover:translate-x-1 font-semibold hover:bg-none">
+                        <a class="flex items-center gap-1 font-semibold" href="#${text.toLowerCase()}">
                             ${text}
                             <i class="fa-solid fa-arrow-right -rotate-45 hover:rotate-90 transition-all duration-300"></i>
                         </a>
